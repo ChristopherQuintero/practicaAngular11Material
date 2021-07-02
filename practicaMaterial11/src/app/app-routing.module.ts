f913a5229622545a8a48f776e2475ad62d4a6967
Christopher Alexander Quintero Guerrero <cquinterog9@gmail.com>
2021-07-02 11:22:52 -0400

    update proyect

--- practicaMaterial11/src/app/app-routing.module.ts
+++ practicaMaterial11/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+/* import { NoPageFoundComponent } from './no-page-found/no-page-found.component'; */
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '', pathMatch: 'full' },
+  { path: 'about', component: AboutComponent },
+  /* { path: '**', component: NoPageFoundComponent } */
+  { path: '**', redirectTo: '', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
